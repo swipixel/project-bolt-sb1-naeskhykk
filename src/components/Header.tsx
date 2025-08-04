017d016c3da890026e41d3818bd0933d9bcb9432
swipixel <swipixel@gmail.com>
2025-08-04 21:00:54 +0200

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -33,7 +33,7 @@ const Header = () => {
       <nav className="container mx-auto px-6 py-4">
         <div className="flex items-center justify-between">
           <ScrollAnimation animation="fade-in-left">
-            <img src="/Image/logo.jpg" className="h-10"/>
+            <img src="/Image/logo.jpg" className="h-15"/>
           </ScrollAnimation>
           
           {/* Desktop Menu */}
