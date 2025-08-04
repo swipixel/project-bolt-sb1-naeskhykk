13247ae8346cfa23c2b078246fa9137abeb2755d
swipixel <swipixel@gmail.com>
2025-08-04 19:48:15 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,32 @@
-import React from 'react';
-import { AnimationProvider } from './components/AnimationProvider';
-import CustomCursor from './components/CustomCursor';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Menu from './components/Menu';
-import Events from './components/Events';
-import LiveDock from './components/LiveDock';
-import SpotifySection from './components/SpotifySection';
-import Reservation from './components/Reservation';
-import Footer from './components/Footer';
-
-function App() {
-  return (
-    <AnimationProvider>
-      <div className="app-background text-ecru">
-        <CustomCursor />
-        <Header />
-        <Hero />
-        <Menu />
-        <Events />
-        {/*<LiveDock /> */}
-        <SpotifySection />
-        <Reservation />
-        <Footer />
-      </div>
-    </AnimationProvider>
-  );
-}
-
-export default App;
+ import React from 'react';
+ import { AnimationProvider } from './components/AnimationProvider';
+ import CustomCursor from './components/CustomCursor';
+ import Header from './components/Header';
+ import Hero from './components/Hero';
+ import Menu from './components/Menu';
+ import Events from './components/Events';
+ import LiveDock from './components/LiveDock';
+ import SpotifySection from './components/SpotifySection';
+ import Reservation from './components/Reservation';
+ import Footer from './components/Footer';
+ 
+ function App() {
+   return (
+     <AnimationProvider>
+       <div className="app-background text-ecru">
+         <CustomCursor />
+         <Header />
+         <Hero />
+         <Menu />
+         <Events />
+         {/*<LiveDock /> */}
+-        <SpotifySection />
+         <Reservation />
++        <SpotifySection />
+         <Footer />
+       </div>
+     </AnimationProvider>
+   );
+ }
+ 
+ export default App;
